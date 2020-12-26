9d9485f9dcd4be0798329c6fb48eba2e830cbc11
jasontan0649 <jasontankaiyuan@gmail.com>
2020-12-26 10:07:07 +0800

    ggg

--- src/Initializer.java
+++ src/Initializer.java
@@ -3,5 +3,30 @@ public class Initializer {
         Customer.Deserialize();
         Shop.Deserialize();
         Visit.Deserialize();
+
+        if(Customer.custs.isEmpty())
+            preCust();
+
+        if(Shop.shops.isEmpty())
+            preShop();
+
+
     }
+
+    public static void preCust() {
+        new Customer("Johnson Smith","jsmith","0123456789","Normal");
+        new Customer("Jackson Teng","jteng","0124567890","Normal");
+        new Customer("Ahmad Mohd","mohdamd","0191234567","Normal");
+        new Customer("Tan Ah Beng","ahbeng","0133456789","Normal");
+        new Customer("Lim Ah Chong","ahchong","0183456789","Normal");
+    }
+
+    public static void preShop() {
+        new Shop("KFC", "delicious","0312345678","Normal","Ken");
+        new Shop("Texas", "chicken","0322345678","Normal","John");
+        new Shop("Basket Robbin", "icecream","0332345678","Normal","Smith");
+        new Shop("llaollao", "llaollao","0342345678","Normal","Elson");
+    }
+
+
 }
