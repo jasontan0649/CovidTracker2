6eeb2996e65f08ab87614cc1f7f1d780cdccf806
jasontan0649 <jasontankaiyuan@gmail.com>
2020-12-26 11:03:00 +0800

    Randomizer done

--- src/Initializer.java
+++ src/Initializer.java
@@ -1,3 +1,9 @@
+import java.sql.Time;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+
 public class Initializer {
     public static void init() {
         Customer.Deserialize();
@@ -9,6 +15,9 @@ public class Initializer {
 
         if(Shop.shops.isEmpty())
             preShop();
+
+        if(Visit.visits.isEmpty())
+            preVisit();
 
 
     }
@@ -26,6 +35,39 @@ public class Initializer {
         new Shop("Texas", "chicken","0322345678","Normal","John");
         new Shop("Basket Robbin", "icecream","0332345678","Normal","Smith");
         new Shop("llaollao", "llaollao","0342345678","Normal","Elson");
+    }
+
+    public static void preVisit() {
+        LocalDateTime[] randomDTs = timeGene();
+        final Random random = new Random();
+        int CustAmt = Customer.custs.size();
+        int ShopAmt = Shop.shops.size();
+
+        for (int i = 0; i < 30; i++) {
+            Customer randomCust = Customer.custs.get(random.nextInt(CustAmt));
+            Shop randomShop = Shop.shops.get(random.nextInt(ShopAmt));
+            new Visit(randomDTs[i], randomCust.getId(), randomShop.getId());
+        }
+    }
+
+    public static LocalDateTime[] timeGene() {
+        final Random random = new Random();
+        final int millisInDay = 24*60*60*1000;
+
+        LocalDateTime[] randomDTs = new LocalDateTime[30];
+        long[] diffSec = new long[30];
+        LocalDateTime dt = LocalDateTime.now();
+        long curTime = dt.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        for (int i = 0; i < 30; i++)
+            diffSec[i] = random.nextInt(millisInDay);
+        Arrays.sort(diffSec);
+        for (int i = 0; i < 30; i++) {
+            long newDT = curTime - diffSec[29-i];
+            randomDTs[i] = LocalDateTime.ofInstant(Instant.ofEpochMilli(newDT), ZoneId.systemDefault());
+        }
+
+        return randomDTs;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Initializer.init(); //Deserialize aka load all the dat
 
-        Customer c1 = new Customer("T","1","2","N");
-        Shop c2 = new Shop("n","2","2","s","w");
-        Visit n1 = new Visit(c1.getId(),1);
-        Visit n2 = new Visit(c1.getId(),1);
-        Visit n3 = new Visit(c1.getId(),1);
-
-        Report rpt = new Report(c1);
-        rpt.display();
-        rpt.exportCSV();
-
     }
 }
